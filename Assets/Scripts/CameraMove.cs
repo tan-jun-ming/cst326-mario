1fb4a378778a98e8d147cf720638a801440853dd
Tan Jun Ming <jtan@csumb.edu>
2020-02-12 00:40:46 -0800

    Add stuff

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            gameObject.transform.position += Vector3.left * 0.5f;
+        } else if (Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.position += Vector3.right * 0.5f;
+
+        }
+    }
+}
